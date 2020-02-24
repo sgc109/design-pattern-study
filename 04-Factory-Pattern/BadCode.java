7f4d3f853812056da953a195c429d58504d38434
Seongho Hwang <sgc109109@gmail.com>
2020-02-24 22:28:46 +0900

    Create BadCode.java

---
+++ 04-Factory-Pattern/BadCode.java
@@ -0,0 +1,32 @@
+package factory;
+
+class Pizza {
+    void prepare() {}
+    void bake() {}
+    void cut() {}
+    void box() {}
+}
+
+class CheesePizza extends Pizza {}
+class GreekPizza extends Pizza {}
+class PepperoniPizza extends Pizza {}
+
+class PizzaStore {
+    Pizza orderPizza(String type) {
+        Pizza pizza;
+
+        if (type.equals("cheese")) {
+            pizza = new CheesePizza();
+        } else if (type.equals("greek")) {
+            pizza = new GreekPizza();
+        } else if (type.equals("pepperoni")) {
+            pizza = new PepperoniPizza();
+        }
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+        return pizza;
+    }
+}
