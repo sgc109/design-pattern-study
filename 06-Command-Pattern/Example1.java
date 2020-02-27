7751edfcd3c82f9e5a7bc6c023b008bb0d589698
Seongho Hwang <sgc109109@gmail.com>
2020-02-28 01:23:32 +0900

    Create Example1.java

---
+++ 06-Command-Pattern/Example1.java
@@ -0,0 +1,105 @@
+package command;
+
+interface Command {
+    void execute();
+}
+
+class NoCommand implements Command {
+    public void execute() {}
+}
+
+class RemoteControl {
+    Command[] onCommands;
+    Command[] offCommands;
+
+    public RemoteControl() {
+        onCommands = new Command[7];
+        offCommands = new Command[7];
+
+        Command noCommand = new NoCommand();
+        for (int i = 0; i < 7; i++) {
+            onCommands[i] = noCommand;
+            offCommands[i] = noCommand;
+        }
+    }
+
+    public void setCommand(int slot, Command onCommand, Command offCommand) {
+        onCommands[slot] = onCommand;
+        offCommands[slot] = offCommand;
+    }
+
+    public void onButtonWasPushed(int slot) {
+        onCommands[slot].execute();
+    }
+
+    public void offButtonWasPushed(int slot) {
+        offCommands[slot].execute();
+    }
+
+    public String toString() {
+        StringBuffer stringBuff = new StringBuffer();
+        stringBuff.append("\n------ Remote Control ------\n");
+        for (int i = 0; i < onCommands.length; i++) {
+            stringBuff.append("[slot " + i + "] " + onCommands[i].getClass().getName()
+                    + "         " + offCommands[i].getClass().getName() + "\n");
+        }
+        return  stringBuff.toString();
+    }
+}
+
+class Light {
+    String roomName;
+
+    public Light(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public void on() {
+        System.out.println(roomName + " light on");
+    }
+    public void off() {
+        System.out.println(roomName + " light off");
+    }
+}
+
+class LightOffCommand implements Command {
+    Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.off();
+    }
+}
+
+class LightOnCommand implements Command {
+    Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.on();
+    }
+}
+
+public class Example1 {
+    public static void main(String[] args) {
+        RemoteControl remoteControl = new RemoteControl();
+
+        Light livingRoomLight = new Light("Living Room");
+        LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+        LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+
+        remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+
+        remoteControl.onButtonWasPushed(0);
+
+        System.out.println(remoteControl);
+
+        remoteControl.offButtonWasPushed(0);
+    }
+}
