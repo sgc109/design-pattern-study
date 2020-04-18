77f99f0e3eee1a6833ee6325aa3a4e84204b0d61
sungho <sgc109109@gmail.com>
2020-04-18 22:00:22 +0900

    add remoteproxy source codes

---
+++ 11-Proxy-Pattern/remoteproxy/GumballMachineRemote.java
@@ -0,0 +1,11 @@
+package headfirst.designpatterns.proxy.gumball;
+
+import java.rmi.*;
+
+public interface GumballMachineRemote extends Remote {
+    public int getCount() throws RemoteException;
+
+    public String getLocation() throws RemoteException;
+
+    public State getState() throws RemoteException;
+}
