35d53d046643b599b969f8b2a658831c1267285e
sungho <sgc109109@gmail.com>
2020-04-18 21:58:26 +0900

    add source codes

--- 11-Proxy-Pattern/protectionproxy/PersonBean.java
+++ 11-Proxy-Pattern/protectionproxy/PersonBean.java
@@ -1,15 +1,21 @@
 package headfirst.designpatterns.proxy.javaproxy;
 
 public interface PersonBean {
- 
-	String getName();
-	String getGender();
-	String getInterests();
-	int getHotOrNotRating();
- 
+
+    String getName();
+
+    String getGender();
+
+    String getInterests();
+
+    int getHotOrNotRating();
+
     void setName(String name);
+
     void setGender(String gender);
+
     void setInterests(String interests);
-    void setHotOrNotRating(int rating); 
- 
+
+    void setHotOrNotRating(int rating);
+
 }
